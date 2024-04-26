4ed707316a7eca381e197f763469643a4ce59687
nmoerman <nicolas.moerman@hotmail.fr>
2024-04-26 14:18:49 +0200

    feat: create stock list and shop list in database

--- app/src/main/java/com/nmn/foodmanager/shoplist/ShoppingListItemViewModel.kt
+++ app/src/main/java/com/nmn/foodmanager/shoplist/ShoppingListItemViewModel.kt
@@ -6,8 +6,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.nmn.foodmanager.main.FoodmanagerDatabase
+import com.nmn.foodmanager.main.entity.ShoppingList
 import com.nmn.foodmanager.main.entity.ShoppingListItem
 import com.nmn.foodmanager.main.offlineRepository.OfflineShoppingListItemRepository
+import com.nmn.foodmanager.main.offlineRepository.OfflineShoppingListRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -15,10 +17,14 @@ class ShoppingListItemViewModel(application: Application)  : ViewModel() {
 
     val allListItems: LiveData<List<ShoppingListItem>>
     private val repository : OfflineShoppingListItemRepository
+    private val repositoryShoppingList : OfflineShoppingListRepository
 
     init {
         val shoppingListItemDao = FoodmanagerDatabase.getDatabase(application).shoppingListItemDao()
+        val shoppingListDao = FoodmanagerDatabase.getDatabase(application).shoppingListDao()
         repository = OfflineShoppingListItemRepository(shoppingListItemDao)
+        repositoryShoppingList = OfflineShoppingListRepository(shoppingListDao)
+        createListShopping(1)
         allListItems = repository.getAllShoppingListItemsByListStream(1).asLiveData()
     }
 
@@ -37,6 +43,12 @@ class ShoppingListItemViewModel(application: Application)  : ViewModel() {
     fun removeShoppingListItem(shoppingListItem: ShoppingListItem) {
         viewModelScope.launch(Dispatchers.IO) {
             repository.deleteShoppingListItem(shoppingListItem)
+        }
+    }
+
+    private fun createListShopping(id: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repositoryShoppingList.insertShoppingList(ShoppingList(id, "stocklist"))
         }
     }
 }
--- app/src/main/java/com/nmn/foodmanager/stock/StockItemViewModel.kt
+++ app/src/main/java/com/nmn/foodmanager/stock/StockItemViewModel.kt
@@ -7,7 +7,9 @@ import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.nmn.foodmanager.main.FoodmanagerDatabase
 import com.nmn.foodmanager.main.entity.StockItem
+import com.nmn.foodmanager.main.entity.StockList
 import com.nmn.foodmanager.main.offlineRepository.OfflineStockItemRepository
+import com.nmn.foodmanager.main.offlineRepository.OfflineStockListRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -16,10 +18,14 @@ class StockItemViewModel(application: Application)  : ViewModel() {
 
     val allStockItems: LiveData<List<StockItem>>
     private val repository : OfflineStockItemRepository
+    private val repositoryStockList : OfflineStockListRepository
 
     init {
         val stockItemDao = FoodmanagerDatabase.getDatabase(application).stockItemDao()
+        val stockListDao = FoodmanagerDatabase.getDatabase(application).stockListDao()
         repository = OfflineStockItemRepository(stockItemDao)
+        repositoryStockList = OfflineStockListRepository(stockListDao)
+        createListStock(1)
         allStockItems = repository.getAllStockItemsByStockStream(1).asLiveData()
     }
 
@@ -38,6 +44,12 @@ class StockItemViewModel(application: Application)  : ViewModel() {
     fun removeStockItem(stockItem: StockItem) {
         viewModelScope.launch(Dispatchers.IO) {
             repository.deleteStockItem(stockItem)
+        }
+    }
+
+    private fun createListStock(id: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repositoryStockList.insertStockList(StockList(id, "stocklist"))
         }
     }
 }
